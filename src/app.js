8e70c87dff23c1c945b694ea5295292043a706b0
Tcress1993 <cresswellt54@gmail.com>
2025-04-29 14:32:58 -0600

    uppload files

--- src/app.js
+++ src/app.js
@@ -1,19 +1,15 @@
-import './App.css';
-import MovieHeader from './components/movieheader';
-import MovieList from './components/movielist';
-import Movie from './components/movie';
+import './app.css';
+import Calendar from './components/calendar';
 import Authentication from './components/authentication';
+
 import {HashRouter, Routes,  Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <HashRouter> {/* The Router component */}
-        <MovieHeader />
+        <Calendar/>
         <Routes>
-          <Route path="/" element={<MovieList />} />
-          <Route path="/movielist" element={<MovieList />}/>
-          <Route path="/movie/:movieId" element={<Movie />}/>
           <Route path="/signin" element={<Authentication />}/>
           {/*... other routes */}
         </Routes>
