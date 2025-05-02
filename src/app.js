49844ad1aec4267108c5fed9bb1e860c99e2d93b
Tcress1993 <cresswellt54@gmail.com>
2025-05-02 05:44:18 -0600

    fix calander and login showing at the same time

--- src/app.js
+++ src/app.js
@@ -3,18 +3,32 @@ import Calendar from './components/calendar';
 import Authentication from './components/authentication';
 
 import {HashRouter, Routes,  Route} from 'react-router-dom';
+import {HashRouter, Routes, Route, Navigate} from 'react-router-dom';
+import {useState, useEffect} from 'react';
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  //make sure the first thing the user sees is the log in screen and not the calendar
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      setIsAuthenticated(true);
+    } else {
+      setIsAuthenticated(false);
+    }
+  }, []);
+  
   return (
     <div className="App">
       <HashRouter> {/* The Router component */}
         <Calendar/>
         <Routes>
-          <Route path="/" element={<Authentication/>} />
-          <Route path="/signin" element={<Authentication />}/>
-          <Route path="/register" element={<Authentication />}/>
-          <Route path="/calendar" element={<Calendar />}/>
-          {/*... other routes */}
+          {/* only navigate to the calender route if the user is authenticated */}
+          <Route path = '/' element = {isAuthenticated ? <Navigate to={"/calendar"} /> : <Authentication setIsAuthenticated={setIsAuthenticated} />} />
+          <Route path = '/signin' element = {isAuthenticated ? <Navigate to={"/calendar"} /> : <Authentication setIsAuthenticated={setIsAuthenticated} />} />
+          <Route path = '/register' element = {isAuthenticated ? <Navigate to={"/calendar"} /> : <Authentication setIsAuthenticated={setIsAuthenticated} />} />
+          <Route path = '/calendar' element ={isAuthenticated ? <Calendar /> : <Navigate to={"/signin"} />} />
         </Routes>
       </HashRouter>
     </div>
