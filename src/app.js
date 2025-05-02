cbd697c20b7e4a0ad873d5a065483fe5f5284ede
Tcress1993 <cresswellt54@gmail.com>
2025-05-02 06:56:39 -0600

    typo

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './app.css';
 import Header from './components/header';
 import Calendar from './components/calendar';
 import Authentication from './components/authentication';
