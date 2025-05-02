08e6694b0c4f2a7f206d64055e63ef053e5c5a25
Tcress1993 <cresswellt54@gmail.com>
2025-05-02 06:53:28 -0600

    add headers

--- src/app.js
+++ src/app.js
@@ -1,50 +1,18 @@
-import './app.css';
+import './App.css';
+import Header from './components/header';
 import Calendar from './components/calendar';
 import Authentication from './components/authentication';
-
-import {HashRouter, Routes,  Route, Navigate, Link} from 'react-router-dom';
-
-import {useState, useEffect} from 'react';
+import {HashRouter, Routes,  Route} from 'react-router-dom';
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  //make sure the first thing the user sees is the log in screen and not the calendar
-  useEffect(() => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      setIsAuthenticated(true);
-    } else {
-      setIsAuthenticated(false);
-    }
-  }, []);
-
-  const handleLogout =() => {
-    localStorage.removeItem('token');
-    localStorage.removeItem('username');
-    setIsAuthenticated(false);
-  }
-  
   return (
     <div className="App">
-      <HashRouter>
-        <nav className = "banner">
-          <ul className = "nav-links">
-            <li>
-              <Link to='/signin' className = {!isAuthenticated ? 'active' : ''}>
-                {isAuthenticated ? 'Logout' : 'Login'}
-              </Link>
-            </li>
-            <li>
-              <Link to={'/calendar'} className = {isAuthenticated ? '' : 'disabled'} onClick = {(e)=> {if (!isAuthenticated) e.preventDefault();}}>
-                Calendar
-              </Link>
-            </li>
-          </ul>
-        </nav>
+      <HashRouter> {/* The Router component */}
+        <Header />
         <Routes>
           <Route path="/" element={<Calendar />} />
           <Route path="/signin" element={<Authentication />}/>
+          {/*... other routes */}
         </Routes>
       </HashRouter>
     </div>
---
+++ src/components/headers.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { logoutUser } from "../actions/authActions";
+
+function Header() {
+    const dispatch = useDispatch();
+    const loggedIn = useSelector((state) => state.auth.loggedIn);
+    const username = useSelector((state) => state.auth.username);
+    const selectedMovie = useSelector((state) => state.movie.selectedMovie);
+    
+    const logout = () => {
+        dispatch(logoutUser());
+    };
+
+    return (
+        <div>
+            <Navbar expand="lg" bg="dark" variant="dark">
+                <Navbar.Brand as={NavLink} to="/">Calendar</Navbar.Brand> 
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ml-auto">
+                    <Nav.Link as={NavLink} to="/Calendar" disabled={!loggedIn}> 
+                        Movie List
+                    </Nav.Link>
+                    <Nav.Link as={NavLink} to="/signin"> 
+                        {loggedIn? (
+                        <span onClick={logout} style={{ cursor: 'pointer' }}>
+                            Logout
+                        </span>
+                        ): (
+                        'Login'
+                        )}
+                    </Nav.Link>
+                </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </div>
+    );
+}
+
+export default Header;
